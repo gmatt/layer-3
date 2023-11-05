bafdf0e1fd8c5cce84c49eb5e1c317085278b580
Matt Sárdi <sardi.matt@gmail.com>
2023-11-05 18:33:12 +0100

    style(model): cleanup main_3.py

--- layer_3/main_3.py
+++ layer_3/main_3.py
@@ -1,35 +1,21 @@
+import numpy as np
 import torch
 from PIL import Image
-
+from lavis.common.gradcam import getAttMap
 from lavis.models import load_model_and_preprocess
+from lavis.models.blip_models.blip_image_text_matching import compute_gradcam
+from matplotlib import pyplot as plt
 
-# %% md
-#### Load an example image and text
-# %%
 raw_image = Image.open("../docs/_static/merlion.png").convert("RGB")
-display(raw_image.resize((596, 437)))
-# %%
-# setup device to use
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# %%
+
 caption = "Merlion near marina bay."
-# %% md
-#### Load model and preprocessors
-# %%
-# model, vis_processors, text_processors = load_model_and_preprocess("blip_image_text_matching", "base", device=device, is_eval=True)
 model, vis_processors, text_processors = load_model_and_preprocess(
     "blip_image_text_matching",
     "large",
     device=device,
     is_eval=True,
 )
-# %% md
-#### Plot utilities for GradCam
-# %%
-from matplotlib import pyplot as plt
-from lavis.common.gradcam import getAttMap
-from lavis.models.blip_models.blip_image_text_matching import compute_gradcam
-import numpy as np
 
 dst_w = 720
 w, h = raw_image.size
@@ -37,28 +23,13 @@ scaling_factor = dst_w / w
 
 resized_img = raw_image.resize((int(w * scaling_factor), int(h * scaling_factor)))
 norm_img = np.float32(resized_img) / 255
-# %% md
-#### Preprocess image and text inputs
-# %%
 img = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
 txt = text_processors["eval"](caption)
-# %% md
-#### Compute GradCam
-# %%
 txt_tokens = model.tokenizer(txt, return_tensors="pt").to(device)
 gradcam, _ = compute_gradcam(model, img, txt, txt_tokens, block_num=7)
-# %% md
-#### Average GradCam for the full image
-# %%
 avg_gradcam = getAttMap(norm_img, gradcam[0][1], blur=True)
-# %%
-# fig, ax = plt.subplots(num_image, 1, figsize=(15,5*num_image))
 fig, ax = plt.subplots(1, 1, figsize=(10, 10))
 ax.imshow(avg_gradcam)
-
-# %% md
-#### GradCam for each token
-# %%
 num_image = len(txt_tokens.input_ids[0]) - 2
 fig, ax = plt.subplots(num_image, 1, figsize=(15, 5 * num_image))
 
