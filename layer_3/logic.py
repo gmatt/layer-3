dc8919174a089308242f9946080699b4d2ca48e9
Matt Sárdi <sardi.matt@gmail.com>
2023-11-06 18:29:17 +0100

    feat(logic): first complete solution, WIP

--- layer_3/logic.py
+++ layer_3/logic.py
@@ -15,7 +15,8 @@ VQA_URL = "http://localhost:8002/generate"
 LOCALIZE_URL = "http://localhost:8001/localize"
 
 # PROMPT = "I want to perform the following action: {action}. I currently see this screen. What should I click on next?"
-PROMPT = "Question: I am trying to {action}. On what should I click next? Answer:"
+# PROMPT = "Question: I am trying to {action}. Where should I click next? Answer:"
+PROMPT = "Task: {action}, Choices: click <button name>, press <keyboard key>, type <text>, done. Answer:"
 
 
 def pil_to_bytesio(image: Image) -> BytesIO:
@@ -26,43 +27,58 @@ def pil_to_bytesio(image: Image) -> BytesIO:
 
 
 def perform_action(action: str) -> None:
-    screen_width, screen_height = pyautogui.size()
-    logging.info("screen_width=%r screen_height=%r", screen_width, screen_height)
+    try:
+        screen_width, screen_height = pyautogui.size()
+        logging.info("screen_width=%r screen_height=%r", screen_width, screen_height)
 
-    logging.info("Creating screenshot")
-    image: Image = pyautogui.screenshot()
-    logging.info("Created screenshot")
-    # Resize image, this is useful for retina resolution.
-    image = image.resize((screen_width, screen_height))
+        for i in range(5):
+            logging.info("Creating screenshot")
+            image: Image = pyautogui.screenshot()
+            logging.info("Created screenshot")
+            # Resize image, this is useful for retina resolution.
+            image = image.resize((screen_width, screen_height))
 
-    prompt = PROMPT.format(action=action)
+            prompt = PROMPT.format(action=action)
 
-    logging.info("Asking model %r...", prompt)
-    response = requests.post(
-        VQA_URL,
-        files={"image": pil_to_bytesio(image)},
-        data={"question": prompt},
-    )
-    response.raise_for_status()
+            logging.info("Asking model %r...", prompt)
+            response = requests.post(
+                VQA_URL,
+                files={"image": pil_to_bytesio(image)},
+                data={"question": prompt},
+            )
+            response.raise_for_status()
 
-    answer = response.json()["answer"]
+            answer: str = response.json()["answer"]
 
-    logging.info("Got answer %r", answer)
+            logging.info("Got answer %r", answer)
 
-    logging.info("Asking to locate %r...", answer)
+            if answer.strip().lower().startswith("click"):
+                answer = answer.strip().lower().replace("click", "").strip()
+                logging.info("Asking to locate %r...", answer)
+                response = requests.post(
+                    LOCALIZE_URL,
+                    files={"image": pil_to_bytesio(image)},
+                    data={"label": answer},
+                )
+                response.raise_for_status()
+                coordinates = response.json()
+                logging.info("Got coordinates %r", coordinates)
+                pyautogui.click(coordinates["x"], coordinates["y"])
+            elif answer.strip().lower().startswith("type"):
+                answer = answer.strip().lower().replace("type", "").strip()
+                logging.info("Typing %r", answer)
+                pyautogui.write(answer, interval=0.25)
+            elif answer.strip().lower().startswith("press"):
+                answer = answer.strip().lower().replace("press", "").strip()
+                logging.info("pressing %r", answer)
+            elif answer.strip().lower().startswith("done"):
+                logging.info("Received done", answer)
+                return
+            else:
+                logging.info("Unknown result: %r", answer)
 
-    response = requests.post(
-        LOCALIZE_URL,
-        files={"image": pil_to_bytesio(image)},
-        data={"label": answer},
-    )
-    response.raise_for_status()
-
-    coordinates = response.json()
-
-    logging.info("Got coordinates %r", coordinates)
-
-    pyautogui.click(coordinates["x"], coordinates["y"])
+    except pyautogui.FailSafeException:
+        return
 
 
 if __name__ == "__main__":
